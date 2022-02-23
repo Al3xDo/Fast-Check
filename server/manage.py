871e96d6e098bd13946c122bfbf857cebb4c04fe
Al3xDo <56794124+Al3xDo@users.noreply.github.com>
2022-02-23 10:26:32 +0700

    fix

--- server/manage.py
+++ server/manage.py
@@ -27,6 +27,7 @@ def run():
 @manager.command
 def test():
     """Runs the unit tests."""
+    print(os.getenv("API_ENV"))
     tests = unittest.TestLoader().discover('./app/test', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
